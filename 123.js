c8182e9ae5a3d10855fe5544f76d4c8c0ec0273a
10k20 <10k20.dev@gmail.com>
2023-02-07 18:41:36 +0300

    count of true

---
+++ 123.js
@@ -0,0 +1,21 @@
+const arrayOfSheep
+ = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+function countSheeps(arrayOfSheep) {
+  let trueValues = 0
+  console.log(arrayOfSheep)
+  arrayOfSheep.forEach(item => {
+    if (item === true) {
+      trueValues += 1
+    }
+  })
+  console.log(trueValues)
+}
+
+countSheeps(arrayOfSheep)
+  
