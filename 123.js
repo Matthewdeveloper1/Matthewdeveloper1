c31254ca2b93a93c658a6cdb7ee0b1a175ecfc90
10k20 <10k20.dev@gmail.com>
2023-02-13 13:52:31 +0300

    djhf123

--- 123.js
+++ 123.js
@@ -16,3 +16,19 @@ function countSheeps(arrayOfSheep) {
   })
   console.log(trueValues)
 }
+
+const snail = (array) =>{
+  let finalArray = []
+  while(array.length){
+    finalArray.push(...array.shift())
+    for (var i = 0; i < array.length; i++){
+      finalArray.push(array[i].pop())
+    }
+    finalArray.push(...(array.pop() || []).reverse())
+    for (var i = array.length -1; i >= 0; i--){
+      finalArray.push(array[i].shift())
+    }
+  }
+  
+  return finalArray
+}
